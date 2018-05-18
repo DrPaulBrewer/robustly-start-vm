36018eda0624182767a8d94fe4173ea76a106ce0
Dr Paul Brewer <drpaulbrewer@eaftc.com>
2018-05-18 01:15:37 -0400

    atom: beautify

--- index.js
+++ index.js
@@ -17,75 +17,79 @@ const promiseRetry = require('promise-retry');
 const after = require('after-operation');
 const Boom = require('boom');
 
-function toRegion(z){
-    // remove the dash and the last letter from the zone to get the region
-    return z.replace(/-.$/,'');
+function toRegion(z) {
+  // remove the dash and the last letter from the zone to get the region
+  return z.replace(/-.$/, '');
 }
 
-module.exports = function(gce, safety, timeout){
-    if (typeof(safety)!=="string") throw new Error("safety must be a string");
-    if (safety.length===0) throw new Error("safety string must not be length 0");
-    return function robustlyStartVM({zones,vmname,diskFunc,vmFunc}){
-	if (!safety || !(vmname.startsWith(safety))) throw new Error("bad vmname ${vmname}");
-	if (!Array.isArray(zones)) throw new Error("bad or missing array zones");
-	if (typeof(vmname)!=="string") throw new Error("bad or missing string vmname");
-	if (typeof(diskFunc)!=="function") throw new Error("bad or missing function diskFunc");
-	if (typeof(vmFunc)!=="function") throw new Error("bad or missing function vmFunc");
-	function locationForAttempt(n){
-	    return {'zone':zones[n-1], 'region': toRegion(zones[n-1])};
-	}
-	function diskForAttempt(n){
-	    return diskFunc(locationForAttempt(n));
-	}
-	function vmForAttempt(n){
-	    return vmFunc(locationForAttempt(n));
-	}
-	function deleteOldDisk(n){
-	    const gzone = gce.zone(locationForAttempt(n-1).zone);
-	    return (
-		gzone
-		    .disk(vmname)
-		    .delete()
-		    .catch(ee=>console.log(`Cleanup error on disk deletion ${vmname}: ${ee}. `))
-			);
-	}
-	function attemptStartVM(retry, attemptNumber){
-	    if (attemptNumber===0) throw new Error("promise-rety attemptNumber should never be zero, but it is zero");
-	    if ((attemptNumber>3) || (attemptNumber>zones.length))
-		throw new Error("VM creation failed. Too busy. Please try again later.");
-	    const gzone = gce.zone(locationForAttempt(attemptNumber).zone);
-	    const prep  = (attemptNumber<=1)? Promise.resolve(0): deleteOldDisk(attemptNumber);
-	    return (prep
-		    .then(()=>(gzone.createDisk(vmname, diskForAttempt(attemptNumber))))
-		    .then((data) => {
-			const disk = data[0];
-			const operation = data[1];
-			const apiResponse = data[2];
-			return after(operation, disk);
-		    })
-		    .then((disk) => {
-			return (gzone
-				.createVM(vmname, vmForAttempt(attemptNumber))
-				.then((data) => {
-				    const vm = data[0];
-				    const operation = data[1];
-				    const apiResponse = data[2];
-				    return after(operation, locationForAttempt(attemptNumber) );
-				})
-			       );
-		    })
-		    .catch((e) => {
-			if (e.toString().includes("already exists")){
-			    throw Boom.conflict(e.toString());
-			}
-			if (e.toString().toLowerCase().includes("try a different zone")){
-			    retry(e);
-			}
-			throw e;
-		    })
-			);
-	}
-	return promiseRetry(attemptStartVM, {retries: zones.length-1, minTimeout: (timeout || 30000), factor: 1.5 });
-    }                                                    
-};
+module.exports = function (gce, safety, timeout) {
+  if (typeof (safety) !== "string") throw new Error("safety must be a string");
+  if (safety.length === 0) throw new Error("safety string must not be length 0");
+  return function robustlyStartVM({ zones, vmname, diskFunc, vmFunc }) {
+    if (!safety || !(vmname.startsWith(safety))) throw new Error("bad vmname ${vmname}");
+    if (!Array.isArray(zones)) throw new Error("bad or missing array zones");
+    if (typeof (vmname) !== "string") throw new Error("bad or missing string vmname");
+    if (typeof (diskFunc) !== "function") throw new Error("bad or missing function diskFunc");
+    if (typeof (vmFunc) !== "function") throw new Error("bad or missing function vmFunc");
 
+    function locationForAttempt(n) {
+      return { 'zone': zones[n - 1], 'region': toRegion(zones[n - 1]) };
+    }
+
+    function diskForAttempt(n) {
+      return diskFunc(locationForAttempt(n));
+    }
+
+    function vmForAttempt(n) {
+      return vmFunc(locationForAttempt(n));
+    }
+
+    function deleteOldDisk(n) {
+      const gzone = gce.zone(locationForAttempt(n - 1).zone);
+      return (
+        gzone
+        .disk(vmname)
+        .delete()
+        .catch(ee => console.log(`Cleanup error on disk deletion ${vmname}: ${ee}. `))
+      );
+    }
+
+    function attemptStartVM(retry, attemptNumber) {
+      if (attemptNumber === 0) throw new Error("promise-rety attemptNumber should never be zero, but it is zero");
+      if ((attemptNumber > 3) || (attemptNumber > zones.length))
+        throw new Error("VM creation failed. Too busy. Please try again later.");
+      const gzone = gce.zone(locationForAttempt(attemptNumber).zone);
+      const prep = (attemptNumber <= 1) ? Promise.resolve(0) : deleteOldDisk(attemptNumber);
+      return (prep
+        .then(() => (gzone.createDisk(vmname, diskForAttempt(attemptNumber))))
+        .then((data) => {
+          const disk = data[0];
+          const operation = data[1];
+          const apiResponse = data[2];
+          return after(operation, disk);
+        })
+        .then((disk) => {
+          return (gzone
+            .createVM(vmname, vmForAttempt(attemptNumber))
+            .then((data) => {
+              const vm = data[0];
+              const operation = data[1];
+              const apiResponse = data[2];
+              return after(operation, locationForAttempt(attemptNumber));
+            })
+          );
+        })
+        .catch((e) => {
+          if (e.toString().includes("already exists")) {
+            throw Boom.conflict(e.toString());
+          }
+          if (e.toString().toLowerCase().includes("try a different zone")) {
+            retry(e);
+          }
+          throw e;
+        })
+      );
+    }
+    return promiseRetry(attemptStartVM, { retries: zones.length - 1, minTimeout: (timeout || 30000), factor: 1.5 });
+  }
+};
